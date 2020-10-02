c03b510b27dcfeed6a5aa60cf410c63416680c88
shivam kumar <53653409+shivam2001pandey@users.noreply.github.com>
2020-10-02 17:38:10 +0530

    Update sort_0 1 2.cpp
    This is an effecient approach to arrange 0s,1s,2s in an array using Dutch National Flag Algorithm

--- sort_0 1 2.cpp
+++ sort_0 1 2.cpp
@@ -1,27 +1,20 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-	int n,cnt0=0,cnt1=0,cnt2=0;
-	cin>>n;
-	int a[n];//array consists of 0,1 and 2.
-	for(int i=0;i<n;i++)
-		cin>>a[i];
-	for(int i=0;i<n;i++)
-	{
-		if(a[i]==0)
-			cnt0++;
-		else if(a[i]==1)
-			cnt1++;
-		else
-			cnt2++;
-	}
-	int j=0;
-	while(cnt0--) a[j++]=0;
-	while(cnt1--) a[j++]=1;
-	while(cnt2--) a[j++]=2;
-	for(int i=0;i<n;i++)
-		cout<<a[i]<<" ";
-	return 0;
-}
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0,m=0,h=nums.size()-1;
+        while(m<=h)
+        {
+            if(nums[m]==0)
+            {
+                swap(nums[m],nums[l]);
+                m++;l++;
+            }
+            else if(nums[m]==1) m++;
+            else
+            {
+                swap(nums[h],nums[m]);
+                h--;
+            }
+        }   
+    }
+};
